60d07b20bdbc56144052955912771316c2e64ba3
tize <103060616+tizedodo@users.noreply.github.com>
2024-04-24 20:49:16 -0500

    log get

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
 function mostrarForm(formulario) {
     var formLogin = document.getElementById('formulario-login');
     var formRegistro = document.getElementById('formulario-registro');
+    var cerrar = document.getElementById('cerrar');
   
     if (formulario === 'login') {
       formLogin.style.display = 'block';
       formRegistro.style.display = 'none';
+      cerrar.style.display = 'block';
     } else if (formulario === 'registro') {
       formRegistro.style.display = 'block';
       formLogin.style.display = 'none';
+      cerrar.style.display = 'block';
     }
-
-
-    
   }
   
+
+function cerrar() {
+    var formLogin = document.getElementById('formulario-login');
+    var formRegistro = document.getElementById('formulario-registro');
+    var cerrar = document.getElementById('cerrar');
+
+
+    formRegistro.style.display = 'none';
+    formLogin.style.display = 'none';
+    cerrar.style.display = 'none';
+    
+  
+
+
+}
+   
